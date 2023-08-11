d983cc3819290d8f13a5b04892ad79d8740f2621
Drobot777 <121822078+Drobot777@users.noreply.github.com>
2023-08-11 10:47:02 +0300

    final change

--- src/components/statistics/Statistics.jsx
+++ src/components/statistics/Statistics.jsx
@@ -3,27 +3,19 @@ import css from './Statistics.module.css';
 import {Notification} from 'components/Notification';
 
 export const Statistics = ({good, neutral, bad, total, positivePercentage}) => {
-  const renders = () => {
-    if (total === 0) {
-      return <div><Notification message="There is no feedback" /></div>;
-    } else {
-      return (
-        <div>
-          <p className={css.titlegood}>Good: {good}</p>
-          <p className={css.titleneutral}>Neutral: {neutral}</p>
-          <p className={css.titlebag}>Bad: {bad}</p>
-          <p className={css.titletotal}>Total: {total}</p>
-          <p className={css.titlepositiv}>
-            Positiv feedback: {positivePercentage}%
-          </p>
-        </div>
-      );
-    }
-  };
-
   return (
     <div>
-      {renders ()}
+      {total === 0
+        ? <div><Notification message="There is no feedback" /></div>
+        : <div>
+            <p className={css.titlegood}>Good: {good}</p>
+            <p className={css.titleneutral}>Neutral: {neutral}</p>
+            <p className={css.titlebag}>Bad: {bad}</p>
+            <p className={css.titletotal}>Total: {total}</p>
+            <p className={css.titlepositiv}>
+              Positiv feedback: {positivePercentage}%
+            </p>
+          </div>}
 
     </div>
   );
